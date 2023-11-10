496f0b16a6ac74c526d2ec836ede2c80645899bf
Zyrephus <wilsonchen315@gmail.com>
2023-11-10 12:42:56 -0500

    Automated program

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
 import os
 import shutil
+import schedule
+import time
 
-path = "/Users/wilson/Downloads/"
-desktop = "/Users/wilson/Desktop/"
+def move_files():
+    path = "/Users/wilson/Downloads/"
 
-file_name = os.listdir(path)
+    file_name = os.listdir(path)
 
-# folders
-folder_names = ['pdf files/','png files/', 'jpg and jpeg files/', 'gif files/']
+    # folders
+    folder_names = ['pdf files/','png files/', 'jpg and jpeg files/', 'gif files/']
 
-for loop in range(0, 4):
-    # if the folder doesn't exist in Downloads aready
-    if not os.path.exists(path + folder_names[loop]):
-        # makes the folders in folder_names
-        os.makedirs(path + folder_names[loop])
+    for loop in range(0, 4):
+        # if the folder doesn't exist in Downloads aready
+        if not os.path.exists(path + folder_names[loop]):
+            # makes the folders in folder_names
+            os.makedirs(path + folder_names[loop])
 
 
-# moving file into files
-for file in file_name:
-    # if file is pdf and isn't already in pdf files
-    if '.pdf' in file and not os.path.exists(path + 'pdf files/' + file):
-        shutil.move(path + file, path + 'pdf files/' + file)
-    # pngs
-    elif '.png' in file and not os.path.exists(path + 'png files/' + file):
-        shutil.move(path + file, path + 'png files/' + file)
-    # jpgs and jpegs
-    elif ('.jpg' in file or '.jpeg' in file) and not os.path.exists(path + 'jpg andjpeg files/' + file):
-        shutil.move(path + file, path + 'jpg and jpeg files/' + file)
-    # GIFs
-    elif '.gif' in file and not os.path.exists(path +'gif files/' + file):
-        shutil.move(path + file, path + 'gif files/' + file)
+    # moving file into files
+    for file in file_name:
+        # if file is pdf and isn't already in pdf files
+        if '.pdf' in file and not os.path.exists(path + 'pdf files/' + file):
+            shutil.move(path + file, path + 'pdf files/' + file)
+        # pngs
+        elif '.png' in file and not os.path.exists(path + 'png files/' + file):
+            shutil.move(path + file, path + 'png files/' + file)
+        # jpgs and jpegs
+        elif ('.jpg' in file or '.jpeg' in file) and not os.path.exists(path + 'jpg andjpeg files/' + file):
+            shutil.move(path + file, path + 'jpg and jpeg files/' + file)
+        # GIFs
+        elif '.gif' in file and not os.path.exists(path +'gif files/' + file):
+            shutil.move(path + file, path + 'gif files/' + file)
+
+    # desktop
+    path = "/Users/wilson/Desktop/"
+
+    # screenshots
+    if not os.path.exists(path + 'screenshots/'):
+        os.makedirs(path + 'screenshots/')
+
+    for file2 in os.listdir("/Users/wilson/Desktop"):
+        if 'Screenshot' in file2 and not os.path.exists(path + 'screenshots/' + file2):
+            shutil.move(path + file2, path + 'screenshots/' + file2)
+
+# Schedule the job to run every 1 minute
+schedule.every(1).minutes.do(move_files)
+
+# Run the scheduler in a loop
+while True:
+    schedule.run_pending()
+    time.sleep(1)  # Sleep for 1 second to avoid high CPU usage
